a89d3a4c5329235c09c98c328d0e97083b0a53d1
Felipe Ferreira <100241185+felipeov1@users.noreply.github.com>
2024-11-17 18:28:39 -0300

    feat:  structure of the project

---
+++ src/main/java/com/menumestre/menumestre/MenumestreApplication.java
@@ -0,0 +1,13 @@
+package com.menumestre.menumestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenumestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenumestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menumestre/menumestre/MenumestreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menumestre.menumestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenumestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
